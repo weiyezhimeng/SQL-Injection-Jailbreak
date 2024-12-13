585de563cf5297e999058ee7d3f0eb91efc42cc0
weiyezhimeng <90603433+weiyezhimeng@users.noreply.github.com>
2024-12-13 14:26:35 +0800

    Update defense_utils.py

--- sql_defense/defense_utils.py
+++ sql_defense/defense_utils.py
@@ -1,11 +1,11 @@
 import random
 
 register = {
-    "vicuna":"/public/zhaojiawei/vicuna-7b-v1.5",
-    "llama2":"/public/zhaojiawei/Llama-2-7b-chat-hf",
-    "llama3":"/public/zhaojiawei/Llama-3.1-8B-Instruct",
-    "mistral":"/public/zhaojiawei/Mistral-7B-Instruct-v0.3",
-    "deepseek":"/public/zhaojiawei/deepseek-llm-7b-chat",
+    "vicuna":"vicuna-7b-v1.5",
+    "llama2":"Llama-2-7b-chat-hf",
+    "llama3":"Llama-3.1-8B-Instruct",
+    "mistral":"Mistral-7B-Instruct-v0.3",
+    "deepseek":"deepseek-llm-7b-chat",
 }
 
 def secret_generate(key, vocab_size):
