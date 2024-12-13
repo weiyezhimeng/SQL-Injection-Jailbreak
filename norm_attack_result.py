fcfa6f4caec52c65e1c29270175f890ebee8b442
weiyezhimeng <90603433+weiyezhimeng@users.noreply.github.com>
2024-12-13 17:06:18 +0800

    Update norm_attack_result.py

--- norm_attack_result.py
+++ norm_attack_result.py
@@ -228,10 +228,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="norm")
     args = get_args()
     regester = {
-        "vicuna":"/public/zhaojiawei/vicuna-7b-v1.5",
-        "llama2":"/public/zhaojiawei/Llama-2-7b-chat-hf",
-        "llama3":"/public/zhaojiawei/Llama-3.1-8B-Instruct",
-        "mistral":"/public/zhaojiawei/Mistral-7B-Instruct-v0.3",
-        "deepseek":"/public/zhaojiawei/deepseek-llm-7b-chat",
+        "vicuna":"vicuna-7b-v1.5",
+        "llama2":"Llama-2-7b-chat-hf",
+        "llama3":"Llama-3.1-8B-Instruct",
+        "mistral":"Mistral-7B-Instruct-v0.3",
+        "deepseek":"deepseek-llm-7b-chat",
     }
     eval(args.model_name, regester[args.model_name], args.prompt_path, args.method_name)
